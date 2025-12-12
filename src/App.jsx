90483a9d9a40cf27bc3d6a767b07f36312517b46
Molnár Bence <mbbexonait@gmail.com>
2025-12-12 23:14:54 +0100

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-
-
 import { useState,useEffect } from "react";
 
 export default function App(){
-    const [Characters, setCharacters] = useState([]);
+    const [Characters,setCharacters] = useState([]);
+
 
     useEffect(()=>{
-        async function load() {
-            try{
-                const res = await fetch("https://rickandmortyapi.com/api/character/?page=2");
-
+          async function load(){
+        try{
+            
+                const res = await fetch("https://rickandmortyapi.com/api/character/?page");
                 if(!res.ok){
                     throw new Error("Error" + res.status);
                 }
 
                 const json = await res.json();
-                setCharacters(json.results);
+               setCharacters(json.results);
 
+               console.log(json.results);
+               
                 
-            } catch(error){
-                console.error("API CALL ERROR " + error);
-            }
-        }
 
+
+
+            }catch (error){
+                console.error("Error:", error);
+             } 
+        }
         load();
     },[]);
 
-    return (<div>
-        <h1>Rick and Morty</h1>
-        <p>Karakterek szama: {Characters.length}</p>
-    </div>
-    );
-}
+    return (
+        <div>
+            <h1>R&M</h1>
+            <p>Karakterek szama: {Characters.length}</p>
+        </div>
+    )
+  
+        
+    }
